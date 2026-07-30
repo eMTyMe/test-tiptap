a83d0c6cb96ba30c16f28454fc470bbb1e4de1c7
eMTyMe <max02eu@gmail.com>
2026-07-30 15:33:07 +0200

    added tiptap extensions

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,22 @@
 import './styles.css'
 
 import { TextStyleKit } from '@tiptap/extension-text-style'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
+import Underline from '@tiptap/extension-underline'
+import { Color, BackgroundColor, TextStyle, FontSize } from '@tiptap/extension-text-style'
+import TextAlign from '@tiptap/extension-text-align'
+import { BulletList, OrderedList, ListItem } from '@tiptap/extension-list'
 import { EditorContent, useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { UndoRedo } from '@tiptap/extensions'
 import React from 'react'
-import { MenuBar } from '../components/MenuBar.tsx'
-import { CustomBubbleMenu } from '@/components/BubbleMenu.tsx'
+import { CustomBubbleMenu } from '../components/BubbleMenu.tsx'
 
-const extensions = [TextStyleKit, StarterKit]
+const extensions = [Document, Paragraph, Text, Bold, Italic, Strike, Underline, Color, BackgroundColor, TextStyle, FontSize, TextAlign, BulletList, OrderedList, ListItem, TextStyleKit, UndoRedo]
 
 export default () => {
   const editor = useEditor({
