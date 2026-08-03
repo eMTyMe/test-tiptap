9772de101e750f719323dbfc2fae3df532d5267b
UI Bakery <max@endo7.com>
2026-08-03 07:08:35 +0000

    Added and configured features

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,5 @@
 import './styles.css'
 
-import { TextStyleKit } from '@tiptap/extension-text-style'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
@@ -8,7 +7,7 @@ import Bold from '@tiptap/extension-bold'
 import Italic from '@tiptap/extension-italic'
 import Strike from '@tiptap/extension-strike'
 import Underline from '@tiptap/extension-underline'
-import { Color, BackgroundColor, TextStyle, FontSize } from '@tiptap/extension-text-style'
+import { TextStyle, Color, BackgroundColor, FontSize  } from '@tiptap/extension-text-style'
 import TextAlign from '@tiptap/extension-text-align'
 import { BulletList, OrderedList, ListItem } from '@tiptap/extension-list'
 import { EditorContent, useEditor } from '@tiptap/react'
@@ -16,7 +15,7 @@ import { UndoRedo } from '@tiptap/extensions'
 import React from 'react'
 import { CustomBubbleMenu } from '../components/BubbleMenu.tsx'
 
-const extensions = [Document, Paragraph, Text, Bold, Italic, Strike, Underline, Color, BackgroundColor, TextStyle, FontSize, TextAlign, BulletList, OrderedList, ListItem, TextStyleKit, UndoRedo]
+const extensions = [Document, Paragraph, Text, Bold, Italic, Strike, Underline, TextStyle, Color, BackgroundColor, FontSize, TextAlign, BulletList, OrderedList, ListItem, UndoRedo]
 
 export default () => {
   const editor = useEditor({
@@ -27,8 +26,6 @@ export default () => {
 </h2>
 `,
   })
-
-      //<MenuBar editor={editor} />
   return (
     <>
       <CustomBubbleMenu editor={editor} />
